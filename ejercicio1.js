090930dbc5a1ee1d90f68e05001e48eb27b8f54b
marcos7530 <88636203+marcos7530@users.noreply.github.com>
2023-07-02 20:49:01 -0300

    correccion ejercicio 1,

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,7 +4,6 @@ Se deben mostrar en consola todos los números pares existentes entre el lowerLi
 upperLimitEven con el mensaje “El número <num> es par”. 
 Y mostrar todos los valores impares que existen entre lowerLimitOdd y upperLimitOdd mostrando 
 el mensaje “El número <num> es impar”
-
 */ 
 
 let lowerLimitEven=2;
@@ -16,12 +15,12 @@ let upperLimitOdd=9;
 for (let index = lowerLimitEven; index 
     < upperLimitEven; index++) {
     if (index%2==0) {
-        console.log('El numero ${index} es par');
+        console.log('El numero ' + index +' es par');
     }
 }
 
 for (let index = lowerLimitOdd; index < upperLimitOdd; index++) {
     if (index%2!=0) {
-        console.log('El numero ${index} es imppar');
+        console.log('El numero ' + index +' es imppar');
     }
 }
