1e5bd0a57b76eec0405579e1c7926d58da40b03b
marcos7530 <88636203+marcos7530@users.noreply.github.com>
2023-07-02 20:26:57 -0300

    ejercicio 1 terminado

---
+++ ejercicio1.js
@@ -0,0 +1,26 @@
+/*
+Definir las variables lowerLimitEven, upperLimitEven, lowerLimitOdd y upperLimitOdd. 
+Se deben mostrar en consola todos los números pares existentes entre el lowerLimitEven y 
+upperLimitEven con el mensaje “El número <num> es par”. 
+Y mostrar todos los valores impares que existen entre lowerLimitOdd y upperLimitOdd mostrando 
+el mensaje “El número <num> es impar”
+
+*/ 
+
+let lowerLimitEven=2;
+let upperLimitEven=10;
+
+let lowerLimitOdd=1;
+let upperLimitOdd=9;
+
+for (let index = lowerLimitEven; index < upperLimitEven; index++) {
+    if (index%2==0) {
+        console.log('El numero ${index} es par');
+    }
+}
+
+for (let index = lowerLimitOdd; index < upperLimitOdd; index++) {
+    if (index%2!=0) {
+        console.log('El numero ${index} es imppar');
+    }
+}
